e7ab8b5c36e8dbe01e232ca0bc7701fc4a49e43e
williamrankin <william.rankin01@gmail.com>
2023-12-22 13:21:33 -0700

    updated tests, reduced code, generated readme

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# logo-generator
+# <Logo Generator>
+
+## Description
+As a freelance web developer I want to generate a simple logo for my projects so that I don't have to pay a graphic designer. This project uses node.js and inquirer to produce a simple logo based on user input. 
+
+## Table of Contents (Optional)
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+Node.js, and inquirer are required to run this application.
+
+## Usage
+Open the terminal for the project folder.
+
+Run 'node index' in the terminal.
+
+Follow the prompts
+
+A logo will be generated as "logo.svg"
+
+## Credits
+Tutoring session with Jose Lopez
+
+## License
+
+This repo is covered under the MIT license
+
+## Tests
+
+Run "npm run test" in the terminal to run test for shape classes.
--- index.js
+++ index.js
@@ -1,50 +1,67 @@
-const fs = require('fs');
-const inquirer = require('inquirer')
-const generateLogo = require("./lib/shapes.js")
+const fs = require("fs");
+const inquirer = require("inquirer");
+const { Triangle, Circle, Square } = require("./lib/shapes.js");
 
 const questions = [
-    {
-        type: 'input',
-        name: 'text',
-        message: 'Enter up to 3 letters to be added to your logo.',
+  {
+    type: "input",
+    name: "text",
+    message: "Enter up to 3 letters to be added to your logo.",
+    validate: function (input) {
+      if (input.length > 3) {
+        console.log(" ERROR: Please enter 3 or less letters");
+      } else {
+        return true;
+      }
     },
-    {
-        type: 'input',
-        name: 'textColor',
-        message: 'Enter a color or hexadecimal number for your text.',
-    },
-    {
-        type: 'list',
-        name: 'shape',
-        message: 'Pick a shape for your logo.',
-        choices: ['Circle', 'Triangle', 'Square']
-    },
-    {
-        type: 'input',
-        name: 'shapeColor',
-        message: 'Enter a color or hexadecimal number for your shape.',
-    },
-]
+  },
+  {
+    type: "input",
+    name: "textColor",
+    message: "Enter a color or hexadecimal number for your text.",
+  },
+  {
+    type: "list",
+    name: "shape",
+    message: "Pick a shape for your logo.",
+    choices: ["Circle", "Triangle", "Square"],
+  },
+  {
+    type: "input",
+    name: "shapeColor",
+    message: "Enter a color or hexadecimal number for your shape.",
+  },
+];
 
 const writeToFile = function (fileName, data) {
-    if(!data){
-        return
-    } else {
+  if (!data) {
+    return;
+  } else {
     fs.writeFile(fileName, data, (err) => {
-        if (err) {
-            console.log('error');
-        } else {
-            console.log('Generated logo.svg');
-        }   
-    })
-}
-}
+      if (err) {
+        console.log("error");
+      } else {
+        console.log("Generated logo.svg");
+      }
+    });
+  }
+};
 
 function init() {
-    inquirer
-        .prompt(questions).then((answers) => {
-            writeToFile('logo.svg', generateLogo(answers))
-        })
+  inquirer.prompt(questions).then((answers) => {
+    let choice;
+    if (answers.shape === "Triangle") {
+      choice = new Triangle(answers.shapeColor, answers.text, answers.textColor);
+    }
+    if (answers.shape === "Circle") {
+      choice = new Circle(answers.shapeColor, answers.text, answers.textColor);
+    }
+    if (answers.shape === "Square") {
+        choice = new Square(answers.shapeColor, answers.text, answers.textColor);
+      }
+
+    writeToFile("logo.svg", choice.render());
+  });
 }
 
 init();
